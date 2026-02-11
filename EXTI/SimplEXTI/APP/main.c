e172a5b3fe8efdcd70c6fb44c2429ae55660b802
XIVN1987 <XIVN1987@163.com>
2026-02-11 15:51:58 +0800

    update EXTI demo

--- EXTI/SimplEXTI/APP/main.c
+++ EXTI/SimplEXTI/APP/main.c
@@ -1,17 +1,24 @@
 #include "SWM231.h"
 
 
+#define USE_GPIOA4_IRQ	1
+
+
 int main(void)
-{	
+{
 	SystemInit();
 	
-	GPIO_Init(GPIOA, PIN5, 1, 0, 0, 0);			//输出，接LED
+	GPIO_INIT(GPIOA, PIN5, GPIO_OUTPUT);	//输出，接LED
 	
-	GPIO_Init(GPIOA, PIN4, 0, 1, 0, 0);			//输入，上拉使能，接KEY
+	GPIO_INIT(GPIOA, PIN4, GPIO_INPUT_PullUp);	//输入，上拉使能，接KEY
 	
 	EXTI_Init(GPIOA, PIN4, EXTI_FALL_EDGE);		//下降沿触发中断
 	
+#if USE_GPIOA4_IRQ
 	NVIC_EnableIRQ(GPIOA4_GPIOB4_IRQn);
+#else
+	NVIC_EnableIRQ(GPIOA_IRQn);
+#endif
 	
 	EXTI_Open(GPIOA, PIN4);
 	
@@ -20,9 +27,17 @@ int main(void)
 	}
 }
 
+
+#if USE_GPIOA4_IRQ
 void GPIOA4_GPIOB4_Handler(void)
+#else
+void GPIOA_Handler(void)
+#endif
 {
-	EXTI_Clear(GPIOA, PIN4);
-	
-	GPIO_InvBit(GPIOA, PIN5);
+	if(EXTI_State(GPIOA, PIN4))
+	{
+		EXTI_Clear(GPIOA, PIN4);
+		
+		GPIO_InvBit(GPIOA, PIN5);
+	}
 }
